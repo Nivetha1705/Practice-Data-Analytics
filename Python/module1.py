8b947b362349aec02b19b41cf052dedf9ef0e680
Nivetha1705 <nivetha1755@gmail.com>
2026-07-27 14:22:51 +0530

    Add python practice

---
+++ Python/module1.py
@@ -0,0 +1,21 @@
+class Calculator1:
+    def __init__(self):
+        self.a=20
+        self.b=30
+    def add(self):
+        print(899+345)
+    def sub(self):
+        print(786-245)
+class Calculator2:
+    def mul(self):
+        print(7*6)
+    def div(self):
+        print(789/10)
+c=790
+d=365
+def fun1():
+    print("Inside Fun1")
+def fun2():
+    print("Inside Fun2")
+
+    
