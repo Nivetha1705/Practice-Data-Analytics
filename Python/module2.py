b4021ace5d714b11c2e0041abdd100821d467efe
Nivetha1705 <nivetha1755@gmail.com>
2026-07-28 19:39:59 +0530

    Add Python Practice

---
+++ Python/module2.py
@@ -0,0 +1,33 @@
+class Student1:
+    def __init__(self):
+        self.name="Uresh"
+        self.age=20
+        self.height="5'10"
+    def sleep(self):
+        print("Uresh sleeps like a buffallo")
+    def eat(self):
+        print("Uresh eats less")
+class Student2:
+    def __init__(self):
+        self.name="Praveen"
+        self.age=21
+        self.height="5'2"
+    def walk(self):
+        print("Praveen Walks slowly")
+    def code(self):
+        print("Praveen codes well")
+
+def run():
+    return 25
+def speak():
+    print("People of TN speak Tamil")
+    
+x="TAP ACADEMY"
+y=2345
+z=True
+
+
+    
+    
+        
+        
