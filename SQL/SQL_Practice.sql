e6e091af280b6e7668a1e4aa23cc57af49216377
Nivetha1705 <nivetha1755@gmail.com>
2026-07-29 20:04:56 +0530

    Add SQL Practice Questions

--- SQL/SQL_Practice.sql
+++ SQL/SQL_Practice.sql
@@ -1622,3 +1622,61 @@ select first_name,last_name,allergies
 from patients
 where allergies is not null and city='Hamilton';
 
+17.Show unique birth years from patients and order them by ascending.
+select distinct year(birth_date) as birth_year
+from patients
+order by birth_year;
+
+18.Show unique first names from the patients table which only occurs once in the list.
+For example, if two or more people are named 'John' in the first_name column then dont include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+select first_name
+from patients
+group by first_name
+having count(first_name)=1;
+
+19.Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+select patient_id,first_name
+from patients
+where first_name like "s%s" and len(first_name)>=6;
+
+20.Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.Primary diagnosis is stored in the admissions table.
+select p.patient_id,p.first_name,p.last_name
+from patients as p
+join admissions as a
+on p.patient_id=a.patient_id
+where a.diagnosis='Dementia';
+
+21.Display every patients first_name. Order the list by the length of each name and then by alphabetically.
+select first_name 
+from patients
+order by len(first_name), first_name asc;
+
+22.Show the total amount of male patients and the total amount of female patients in the patients table.
+Display the two results in the same row.
+select(
+select count(Gender)
+from patients
+where gender='M')as male_count,
+(select count(Gender)
+ from patients
+ where gender='F') as female_count;
+Display the two results in the same row.
+
+23.Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
+select first_name,last_name,allergies
+from patients
+where allergies='Penicillin' or allergies='Morphine'
+order by allergies,first_name,last_name;
+
+24.Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis
+select patient_id,diagnosis
+from admissions
+group by patient_id,diagnosis
+having count(diagnosis)>1
+order by patient_id;
+
+25.Show the city and the total number of patients in the city.Order from most to least patients and then by city name ascending.
+select city,count(patient_id) as num_patients
+from patients
+group by city
+order by num_patients desc,city asc;
