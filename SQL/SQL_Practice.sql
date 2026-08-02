61cac9f5a64bb4ed4f5fe22fdb0fc13c94bd6e51
Nivetha1705 <nivetha1755@gmail.com>
2026-08-02 20:51:02 +0530

    Add SQL Practice Questions

--- SQL/SQL_Practice.sql
+++ SQL/SQL_Practice.sql
@@ -1841,3 +1841,11 @@ select count(patient_id) as patients_in_group,
 from patients
 group by weight_group
 order by weight_group desc;
+
+45.Show patient_id, weight, height, isObese from the patients table.Display isObese as a boolean 0 or 1.Obese is defined as weight(kg)/(height(m)2) >= 30.weight is in units kg.Height is in units cm.
+SELECT patient_id, weight, height, 
+  (CASE 
+      WHEN weight/(POWER(height/100.0,2)) >= 30 THEN 1
+      ELSE 0
+      END) AS isObese
+FROM patients;
